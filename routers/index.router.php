af694d961453fec999c027587482a935d720b008
Cesar Redondo <revuls@gmail.com>
2014-05-07 19:28:30 -0400

    Removed some files and dirs

--- routers/index.router.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// GET index route
-$app->get('/', function () use ($app) {
-    $config = $app->config('config');
-    $elements = lib\FExxx::getAllFrom('elements');
-    $app->render('index.html', array('elements' => $elements, 'config' => $config));
-});
